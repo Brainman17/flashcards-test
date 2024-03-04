4cde5aa9c8c483af0ee506dd473e0e061d3f2dc9
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-03-04 18:52:04 +0300

    add validation

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,17 +1,29 @@
 import { useController, useForm } from 'react-hook-form'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
 import s from './login-form.module.scss'
 
 import { Button } from '../../ui/button'
 
-type FormValues = {
-  email: string
-  password: string
-  rememberMe: boolean
-}
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+  rememberMe: z.boolean().default(false),
+})
+
+type FormValues = z.infer<typeof loginSchema>
 
 export const LoginForm = () => {
-  const { control, handleSubmit, register } = useForm<FormValues>()
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormValues>({
+    resolver: zodResolver(loginSchema),
+  })
 
   const {
     field: { onChange, value },
@@ -25,11 +37,17 @@ export const LoginForm = () => {
     console.log(data)
   }
 
+  // const emailRegex =
+  //   /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input {...register('email')} className={s.input} />
+      {errors.email?.message}
       <input {...register('password')} className={s.input} />
+      {errors.password?.message}
       <input {...register('rememberMe')} checked={value} onChange={onChange} type={'checkbox'} />
+      {errors.rememberMe?.message}
       <Button type={'submit'}>Submit</Button>
     </form>
   )
